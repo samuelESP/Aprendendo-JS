6f920c843b50765dedc4399705056d8ea1ac2937
samuelESP <samuelnegocios1509@gmail.com>
2022-07-16 23:27:26 -0300

    create atividades

---
+++ modulos/atividades/funcoes.mjs
@@ -0,0 +1,17 @@
+function mostraIdade(nome, idade) {
+    return `A idade de ${nome} é ${idade}`;
+}
+
+function mostraCidade(nome, cidade) {
+    return `A cidade de ${nome} é ${cidade}`;
+}
+
+function mostraHobby(nome, hobby) {
+    return `A hobby de ${nome} é ${hobby}`;
+}
+
+export {
+    mostraCidade,
+    mostraHobby,
+    mostraIdade
+}
---
+++ modulos/atividades/main.mjs
@@ -0,0 +1,8 @@
+import{ mostraIdade, mostraCidade, mostraHobby} from './funcoes.mjs';
+
+
+console.log(mostraIdade('Samuel', 20));
+
+console.log(mostraCidade('Lucas', 'Curitiba'));
+
+console.log(mostraHobby('Carol', 'dançar'));
